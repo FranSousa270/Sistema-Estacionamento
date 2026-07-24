56162665926b1c1bb9d04e354438e9681a0b3f1c
Cris2700 <cristianoaquino270@gmail.com>
2026-07-24 20:35:20 -0300

    implementa show, create, update e destroy de setores

---
+++ prisma/migrations/20260724232720_add_ativo_to_setor/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Setor" ADD COLUMN     "ativo" BOOLEAN NOT NULL DEFAULT true;
--- src/app/controllers/SetoresControllers.js
+++ src/app/controllers/SetoresControllers.js
@@ -1,58 +1,219 @@
-import prisma from '../../../lib/prisma.js';
+import prisma from "../../../lib/prisma.js";
 
-class SetoresControllers{
-
-   async index(req,res){
-    try{
-        const data = await prisma.setor.findMany();
-        res.status(200).json(data);
-        console.log("GET :: /setores", JSON.stringify(data));
-    } catch(error){
-        message: "Erro interno no servidor"
-    }}
-
-    async show(req,res){
-        const data = await prisma.setor.findUnique({
-            where: {
-                id: parseInt(req.params.id)
-            }
-        });
-        const status = data ? 200 : 404;
-        res.status(status);
-        res.json(data);
+class SetoresControllers {
+  async index(req, res) {
+    try {
+      const data = await prisma.setor.findMany();
+      res.status(200).json(data);
+      console.log("GET :: /setores", JSON.stringify(data));
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
     }
+  }
 
-    async create(req,res){
-        const {nome} = req.body
-        const data = await prisma.setor.create({
-            data: {
-                nome
-            }
+  async show(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+
+      if (isNaN(id)) {
+        return res.status(400).json({
+          message: "Id inválido",
         });
-        res.status(201).json(data);
+      }
+
+      const data = await prisma.setor.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!data) {
+        return res.status(404).json({
+          message: "Setor não encontrado",
+        });
+      }
+      return res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
     }
-    
-    async update(req,res){
-        const {nome} = req.body
+  }
+
+  async create(req, res) {
+    try {
+      let { nome } = req.body;
+
+      if (!nome) {
+        return res.status(400).json({
+          message: "O campo nome do setor deve ser preenchido",
+        });
+      }
+
+      if (typeof nome !== "string") {
+        return res.status(400).json({
+          message: "Escreva um nome válido",
+        });
+      }
+
+      nome = nome.trim();
+
+      const data = await prisma.setor.create({
+        data: {
+          nome,
+        },
+      });
+      res.status(201).json(data);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+      let { nome } = req.body;
+
+      if (isNaN(id)) {
+        return res.status(400).json({
+          message: "Id inválido",
+        });
+      }
+
+      const setor = await prisma.setor.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!setor) {
+        return res.status(404).json({
+          message: "Setor não encontrado",
+        });
+      }
+
+      if (!nome) {
+        return res.status(400).json({
+          message: "O campo nome do setor deve ser preenchido",
+        });
+      }
+
+      if (typeof nome !== "string") {
+        return res.status(400).json({
+          message: "Escreva um nome válido",
+        });
+      }
+
+      nome = nome.trim();
+
+      if (nome.length < 1) {
+        return res.status(400).json({
+          message: "O nome do setor deve ter no minímo 1 carácter",
+        });
+      }
+      const data = await prisma.setor.update({
+        where: {
+          id: parseInt(req.params.id),
+        },
+        data: {
+          nome,
+        },
+      });
+      res.status(200).json(data);
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      const id = parseInt(req.params.id);
+
+      if (isNaN(id)) {
+        return res.status(400).json({
+          message: "Id inválido",
+        });
+      }
+
+      const setor = await prisma.setor.findUnique({
+        where: {
+          id,
+        },
+      });
+
+      if (!setor) {
+        return res.status(404).json({
+          message: "Setor não encontrado",
+        });
+      }
+
+      const vagas = await prisma.vaga.findMany({
+        where: {
+          setorId: id,
+        },
+      });
+
+      let vagasIds = vagas.map((vaga) => vaga.id);
+
+      if (vagas.length === 1) {
         const data = await prisma.setor.update({
-            where: {
-                id: parseInt(req.params.id)
-            },
-            data: {
-                nome
-            }
+          where: {
+            id: parseInt(req.params.id),
+          },
+          data: {
+            ativo: false,
+          },
         });
-        res.status(200).json(data);
-    }
+        return res.status(200).json(data);
+      }
 
-    async destroy(req,res){
-        const data = await prisma.setor.delete({
-            where: {
-                id: parseInt(req.params.id)
-            }
+      const carroEstacionado = await prisma.permanencia.findFirst({
+        where: {
+          vagaId: {
+            in: vagasIds,
+          },
+          saida: null,
+        },
+      });
+
+      if (carroEstacionado) {
+        return res.status(400).json({
+          message: "Não é possível desativar um setor com vagas ocupadas",
         });
-        res.status(200).json(data);
+      }
+
+      await prisma.vaga.updateMany({
+        where: {
+          setorId: id,
+        },
+        data: {
+          ativa: false,
+        },
+      });
+
+      const data = await prisma.setor.update({
+        where: {
+          id,
+        },
+        data: {
+          ativo: false,
+        },
+      });
+
+      return res.status(200).json(data);
+
+    } catch (error) {
+      return res.status(500).json({
+        message: "Erro interno no servidor",
+      });
     }
+  }
 }
 
-export default new SetoresControllers()
+export default new SetoresControllers();
