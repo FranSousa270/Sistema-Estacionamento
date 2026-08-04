2155e8cbb0d0b160467657f9540ad297dae1dae4
Cris2700 <cristianoaquino270@gmail.com>
2026-08-03 21:19:26 -0300

    fix: schema do prisma dos setores

--- src/app/validations/setores.schema.js
+++ src/app/validations/setores.schema.js
@@ -1,5 +1,5 @@
 import { z } from "zod"
 
 export const createSetorSchema = z.object({
-    nome: z.string().trim().min(1, "O nome do setor deve ser preenchido.")
+    nome: z.string().trim().min(1, "O nome do setor deve ser preenchido.").transform((nome) => nome.toUpperCase())
 })
